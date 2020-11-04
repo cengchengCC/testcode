9e86a62029a76c365db2d5cb310548d507fad666
cengchengCC <cengchengCC@163.com>
2020-11-04 20:59:58 +0800

    2020/11/04

--- express/app.js
+++ express/app.js
@@ -1,5 +1,27 @@
+
+'use strict'
+
 const express = require('express')
+const { Sequelize, DataTypes,Model  } = require('sequelize');
+
+const sequelize=new Sequelize('BSdataTest','postgres','616616',{
+	host:'219.216.80.18',
+	port:5432,
+	dialect:'postgres'
+})
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 const app = express()
-const port = 3000
+const port = 2000
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+const roll = require('./models/roll')
+console.log(roll)
+exports.sequelize = sequelize
